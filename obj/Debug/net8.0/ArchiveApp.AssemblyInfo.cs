e6c6bc5bfe6a58387202713e89eeacdb2a974e3b
onuraydinoglu <onur.aydinoglu.98@gmail.com>
2024-12-27 17:15:16 +0300

    Star

--- obj/Debug/net8.0/ArchiveApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ArchiveApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArchiveApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38ad01393ad9797b7e4731fe6a379471deac4992")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4192b3508840f684836ff3fa75975c912d5b7c5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArchiveApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArchiveApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
